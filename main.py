b754acd55d60d3894ca091db4596eeec3e3dc5e9
Chen Harel <59762611+Chen-Harel@users.noreply.github.com>
2022-03-04 00:11:50 +0200

    Added option to delete items from table
    Clear console added for a cleaner experience!

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sqlite3
+import os
+
 try:
   con  = sqlite3.connect('school.db')
 except:
@@ -6,6 +8,13 @@ except:
 
 
 cur = con.cursor()
+
+#clear console
+def clearConsole():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+        command = 'cls'
+    os.system(command)
 
 #update table
 def updateStudent():
@@ -39,42 +48,79 @@ def updateStudent():
       return
 
 # Create table
-def createTable():
+def createStudentsTable():
   try:
     cur.execute(
-        '''CREATE TABLE students (fName text, lName text, Age int, Class, Grade int)''')
+        '''CREATE TABLE students (fName text, lName text, Class int, Age int, Grade int)''')
+    print("Table created!")
   except:
+    pass
+
+#delete table/db
+def delete():
+  tableOrDb = input("Do you want to remove a table or an entire database? ").lower()
+  if tableOrDb == "table":
+    print("THIS ACTION CANNOT BE UNDONE!!!!")
+    whichTable = input("What is the table name? ")
+    cur.execute('''DROP TABLE ?''', (whichTable))
+    print("Table deleted...")
+  elif tableOrDb == "database":
+    print("THIS ACTION CANNOT BE UNDONE!!!!")
+    whichDB = input("What is the database name? ")
+    cur.execute("DROP DATABASE ?", (whichDB))
+
+#remove student from 
+def removeStudent():
+  print("This will remove a student from the database. This CAN'T BE UNDONE!!!")
+  studentFName = input("What is the students first name? ")
+  studentLName = input("What is the students last name? ")
+  studentClass = input("What class is the student in? ")
+  sure = input("Are you sure you want to delete this student? Type 'y' or 'n' ")
+  if sure == "y":
+    print("Student removed from table")
+    cur.execute("DELETE FROM students WHERE fName=? and lName=? and Class=?", (studentFName, studentLName, studentClass))
+  elif sure == "n":
+    print("No changes made.")
     pass
 
 # Insert a row of data
 def addSingleData():
-  cur.execute("INSERT INTO students VALUES(?, ?, ?, ?,?)", (input('Insert the students first name: '), input('Insert the students last name: '), input('What is the students age? '), input('What class is he/she in? '), input('What is his/her grade? ')))
-
+  cur.execute("INSERT INTO students VALUES(?, ?, ?, ?,?)", (input('Insert the students first name: '), input(
+      'Insert the students last name: '), input('What class is he/she in? '), input('What is the students age? '), input('What is his/her grade? ')))
+  print("Table updated with info...")
 #show table
 def showTable():
-  cur.execute("SELECT * FROM students")
-  print(cur.fetchall())
-
-
+  try:
+    cur.execute("SELECT * FROM students")
+    print(cur.fetchall())
+  except:
+    print("No tables to show...")
 
 # Save (commit) the changes
 def save():
   con.commit()
+  closedb = input("Do you want to close the database? Type 'y' or 'n' ")
+  if closedb == "y":
+    print("Database has been closed.")
+    con.close()
+  elif closedb == "n":
+    pass
   # We can also close the connection if we are done with it.
   # Just be sure any changes have been committed or they will be lost.
-  con.close()
-  
+
 def main():
   while(True):
     print("What do you want to do?")
     print("1 - Update a student")
     print("2 - show the current table")
     print("3 - save database")
-    print("4 - create table")
+    print("4 - create students table")
     print("5 - add a single row of data")
-    print("6 - delete a student")
+    print("6 - remove student from DB")
+    print("7 - delete a table/DB")
     print("x - Quit the program")
     userChoice = input().lower()
+    clearConsole()
     if userChoice == "1":
       updateStudent()
     elif userChoice == "2":
@@ -82,16 +128,18 @@ def main():
     elif userChoice == "3":
       save()
     elif userChoice == "4":
-      createTable()
+      createStudentsTable()
     elif userChoice == "5":
       addSingleData()
     elif userChoice == "6":
-      deleteStudent()
+      removeStudent()
+    elif userChoice == "7":
+      print("Currently unavailable")
     elif userChoice == "x":
       print("Goodbye!")
       return
     else:
-      print("Please select an item from the menu list.")
+      print("Please select a number from the menu.")
 
 if __name__ == "__main__":
   main()
@@ -102,8 +150,8 @@ if __name__ == "__main__":
 
 # Insert data from a list
 # manyStudents = [
-  # ("Chen", "Harel", 30, 7731, 100),
-  # ("Ron", "Harel", 25, 123, 90)
+#   ("Chen", "Harel", 30, 7731, 100),
+#   ("Ron", "Harel", 25, 123, 90)
 # ]
 
 #Add many to table
